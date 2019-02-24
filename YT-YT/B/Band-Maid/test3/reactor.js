f364291c017f9fec720f946cfa62b38931dbc401
dlandry <dlandry@ca.ibm.com>
2019-02-24 11:11:21 -0500

    test

--- YT-YT/B/Band-Maid/test3/reactor.js
+++ YT-YT/B/Band-Maid/test3/reactor.js
@@ -15,7 +15,7 @@
           height: '400',
           width: '45%',
           videoId: 'ErCxPH7HQyI',
-          playerVars: { 'enablejsapi' : 1, 'autoplay': 0, 'controls': 0,'rel': 0, 'autohide':1,'wmode':'opaque','origin':'https://reactorhub.github.io' },
+          playerVars: { 'enablejsapi' : 1, 'autoplay': 0, 'controls': 1,'rel': 0, 'autohide':1,'wmode':'opaque','origin':'https://reactorhub.github.io' },
           events: {
             'onReady': onReactorReady,
             'onStateChange': onReactorStateChange
