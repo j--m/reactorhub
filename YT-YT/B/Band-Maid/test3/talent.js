e36d17a13b066827704ed6dc5699297cdf283ed0
dlandry <dlandry@ca.ibm.com>
2019-02-23 19:04:57 -0500

    test

--- YT-YT/B/Band-Maid/test3/talent.js
+++ YT-YT/B/Band-Maid/test3/talent.js
@@ -8,6 +8,7 @@
       // 3. This function creates an <iframe> (and YouTube player)
       //    after the API code downloads.
       var talent;
+      var talenttime = 0;
       function onYouTubeIframeAPIReady() {
         talent = new YT.Player('talent', {
           height: '400',
@@ -22,17 +23,50 @@
 //    setTimeout(function(){
       // 4. The API will call this function when the video player is ready.
       function onTalentReady(event) {
-        //setTimeout(function(){
-        //  event.target.playVideo();
-        //}, 189000)
-        //set up the read localStorage.timeupdater
-        var currentTime
-        talenttimerupdater = setInterval(readTime, 100);
+        function storeTalentTime() {
+          if (typeof(Storage) !== "undefined") {
+            // If there is a local storage timer, then set it to the current time of the reactor video
+            if ( localStorage.talenttimer) {
+               localStorage.talenttimer = talenttime;
+              //localStorage.setItem('reactorPlayer', JSON.stringify(player));
+              //console.log("local storage is: " +  localStorage.reactortimer);
+            } else {
+              // no local storage timer? create it and set it to 0
+               localStorage.talenttimer = 0;
+            }
+          }
+        }
+        // event.target.playVideo();
+        // This function is called every 1/10 sec and updates the
+        // local storage timer with the current time of the reactor video
+        function updateTime() {
+          var oldTime = talenttime;
+          if(talent && talent.getCurrentTime) {
+            talenttime = talent.getCurrentTime();
+
+            // console.log(talenttime);
+            document.getElementById("time").innerHTML = talenttime;
+          }
+          if(talenttime !== oldTime) {
+            onProgress(talenttime);
+          }
+        }
+        // call the updateTime function every 100 ms
+        timeupdater = setInterval(updateTime, 100);
+        // call the storeReactorTime
+        talenttimeupdater = setInterval(storeTalentTime,100)
+      }
+
+      // when the time changes, this will be called.
+      function onProgress(currentTime) {
+        if(currentTime > 20) {
+          console.log("the talent video reached 20 seconds!");
+        }
       }
 
       function readTime() {
         currentTime = localStorage.timer
-        console.log("Local Storage Time is: " + currentTime)
+        console.log("Local Talent Storage Time is: " + currentTime)
       }
       // 5. The API calls this function when the player's state changes.
       //    The function indicates that when playing a video (state=1),
