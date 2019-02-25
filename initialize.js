3e704fa36c8661e06c3399c6a5d14bf5013370ed
dlandry <dlandry@ca.ibm.com>
2019-02-25 09:17:24 -0500

    test

--- initialize.js
+++ initialize.js
@@ -1,4 +1,8 @@
 console.log("Initialize");
+function resizeIframe(obj) {
+  obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+  obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
+}
 var url_string = window.location.href;
 var url = new URL(url_string);
 // This section reads in the various commands and pops them on the storage stack
